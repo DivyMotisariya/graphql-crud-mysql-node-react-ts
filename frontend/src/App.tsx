322bf973e074f470f808eba3dc80ed18ae6522c8
DivyMotisariya <motisariyadivy2@gmail.com>
2021-12-13 16:06:05 +0530

    modules installed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 function App() {
+  const client = new ApolloClient({
+    uri: "http://localhost:8000/graphql",
+    cache: new InMemoryCache(),
+  });
+
   return <div className="App"></div>;
 }
 
